b0bfa6268bff24e693ec5547622f5de893f021b9
Rohit <rohitsi2104@gmail.com>
2025-06-29 01:45:55 +0530

    Fix: static file structure for Vercel

--- FPSDayalbaghBackend/settings.py
+++ FPSDayalbaghBackend/settings.py
@@ -226,10 +226,11 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
