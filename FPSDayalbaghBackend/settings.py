4faebcd80f2c9e4398c1d1bf1c0fdec6733271e8
Rohit <rohitsi2104@gmail.com>
2025-06-29 01:04:52 +0530

    Initial commit 10

--- FPSDayalbaghBackend/settings.py
+++ FPSDayalbaghBackend/settings.py
@@ -173,7 +173,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Must be after SecurityMiddleware
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
